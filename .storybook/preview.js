ba2778ff0f08a8c373ae017aa07c5f7719bb49ab
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-26 14:58:16 +0900

    ✅ add storybook Image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import { RouterContext } from "next/dist/shared/lib/router-context";
+import * as NextImage from "next/image";
 
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+
   nextRouter: {
     Provider: RouterContext.Provider,
   },
