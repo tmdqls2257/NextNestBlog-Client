0d6c500cc330c149abd3774a77b71744ffd10c36
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-30 00:31:55 +0900

    💄 tags

--- pages/blogs/post.tsx
+++ pages/blogs/post.tsx
@@ -1,31 +1,41 @@
 import dynamic from "next/dynamic";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { TagsInput } from "react-tag-input-component";
 
 const EditorComponent = dynamic(() => import("../../elements/ReactQuill"), {
   ssr: false,
 });
 
 // import EditorComponent from "../../elements/ReactQuill";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "react-quill/dist/quill.snow.css";
 import Button from "../../common/button/button";
-import BlogService from "../../service/blogService";
 import { useRouter } from "next/router";
+import BlogService from "service/blogService";
+// import { type } from "../../data/blogData";
+
+export type FormInputs = {
+  title: string;
+  contents: string;
+  description: string;
+  imageUrl: string;
+  tags: string;
+};
 
 const Post = () => {
+  const { register, handleSubmit, control } = useForm<FormInputs>();
+
   const router = useRouter();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [contents, setContents] = useState("");
+  const [tags, setTags] = useState<string[]>([]);
 
-  const onBlurTittle = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onBlurTittle = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.currentTarget.value);
-  };
-
-  const onBlurDescription = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDescription(e.currentTarget.value);
-  };
-
+  }, []);
+  // const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
   const onSubmit = () => {
     setTitle("");
     setDescription("");
@@ -35,9 +45,11 @@ const Post = () => {
       contents,
       description,
       imageUrl,
+      tags,
     };
     BlogService.post(postData);
     router.back();
+    console.log(postData);
   };
 
   return (
@@ -48,7 +60,7 @@ const Post = () => {
       >
         {"title"}
       </label>
-      <input
+      {/* <input
         name="title"
         className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
         id="email"
@@ -57,22 +69,21 @@ const Post = () => {
         onBlur={(e) => {
           onBlurTittle(e);
         }}
-      />
-      <label
-        className="block text-grey-darker text-sm font-bold mb-2"
-        htmlFor="description"
-      >
-        {"description"}
-      </label>
+      /> */}
       <input
-        name="description"
         className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
         id="email"
         type="text"
-        placeholder="description"
-        onBlur={(e) => {
-          onBlurDescription(e);
-        }}
+        placeholder="title"
+        onBlur={onBlurTittle}
+      />
+      {/* <Tag /> */}
+      <TagsInput
+        classNames={{ input: "bg-white" }}
+        value={tags}
+        onChange={setTags}
+        name="fruits"
+        placeHolder="enter fruits"
       />
       {/* <ReactQuill modules={modules} theme="snow" value={content} onChange={setContent} /> */}
       <EditorComponent
