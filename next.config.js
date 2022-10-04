33fd5975f99be3012da2eeaaa7e3ea4cb3dd95bb
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-04 19:54:17 +0900

    📦 add package

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["nextnestblog.s3.ap-northeast-2.amazonaws.com"],
   },
-};
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === "production";
+    const plugins = [...config.plugins];
+    return {
+      ...config,
+      mode: prod ? "production" : "development",
+      devtool: prod ? "hidden-source-map" : "eval",
+      plugins,
+    };
+  },
+});
 
 module.exports = nextConfig;
